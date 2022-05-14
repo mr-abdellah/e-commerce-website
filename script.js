319679ffc89748f1241f7285dfefe2b29ac53b08
mr-abdellah <belkaid.abdulah@gmail.com>
2022-05-14 05:54:32 +0100

    Style Hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const openMenu = document.querySelector('#open-menu-button');
 const closeMenu = document.querySelector('#close-menu-button');
 const navBar = document.querySelector('.navbar');
+const links = document.querySelectorAll('.navbar-link');
 
 openMenu.addEventListener('click', () => {
   openMenu.style.display = 'none';
@@ -12,4 +13,12 @@ closeMenu.addEventListener('click', () => {
   openMenu.style.display = 'block';
   closeMenu.style.display = 'none';
   navBar.style.display = 'none';
+});
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
+    navBar.style.display = 'none';
+  });
 });
